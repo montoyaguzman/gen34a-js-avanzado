125a2c691aa150daaa512c8266fc86750bbca730
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-06-24 21:39:21 -0600

    feat: ejemplo de consumo de APIS con fetch

--- 1-node/1.2-example-fetch/index.js
+++ 1-node/1.2-example-fetch/index.js
@@ -1,1 +1,22 @@
-console.log('esto pasa en el index.js')
+/**
+ * 1. Consultar la pokeapi
+ * 2. Mostrar una lista de nombres de pokemon en el html
+ * 3. Que necesitariamos para consumir el api?
+ *  Hacer un request con base en el CONTRATO del api
+ *   1. url o endpoint
+ *   2. verbo
+ */
+
+const URL = 'https://pokeapi.co/api/v2/pokemon/';
+// GET
+
+async function getData() {
+    const response = await fetch(URL);
+    const pokemons = await response.json();
+    console.log(response);
+    console.log(pokemons);
+}
+
+getData();
+
+
