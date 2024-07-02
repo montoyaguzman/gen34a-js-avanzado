2c44b55b1715168708be45d70b521034ce6b432f
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-07-01 19:44:34 -0600

    feat: avance del api, lectura de url y manejo de error 404

--- 1-node/1.6-web-server/index.js
+++ 1-node/1.6-web-server/index.js
@@ -1,3 +1,43 @@
 // URL 1 localhost:8080 => index.html
 // URL 2 localhost:8080/patito.html => pagina1.html
 // URL 3 localhost:8080/pagina2.html => pagina2.html
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const PORT = 8080;
+
+const server = http.createServer((request, response) => {
+
+    // console.log(request)
+    // console.log(request.url) // o raiz (/), o /patito.html o /pagina2.html
+    const urlObject = url.parse(request.url);
+    const { path } = urlObject; // destructuring
+
+    let fileSystemPath = '';
+    if (path === '/') {
+        fileSystemPath = 'static/index.html';
+    } else {
+        fileSystemPath = `static${path}`; // literal template (backticks)
+    }
+    // console.log('avance:', fileSystemPath);
+
+    fs.stat(fileSystemPath, (error) => {
+        if (!error) {
+            // acceder y regresar la pagina que este invocando con fileSystemPath
+        } else {
+            // si ese fileSystemPath no existe devolver una respuesta 404
+            const status = 404;
+            const mimeTypes = { 'Content-type': 'application/json' };
+            response.writeHead(status, mimeTypes);
+            const responseString = JSON.stringify({ message: 'pagina no encontrada' });
+            response.write(responseString);
+            response.end();
+        }
+    })
+
+
+});
+
+console.log('Servidor iniciado...', PORT);
+server.listen(PORT);
