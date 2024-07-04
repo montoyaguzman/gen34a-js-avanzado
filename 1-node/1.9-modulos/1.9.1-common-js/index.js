60b8af911702823bbeb4567f28c337b98fe223dc
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-07-03 20:51:16 -0600

    feat: modulos en common-js

---
+++ 1-node/1.9-modulos/1.9.1-common-js/index.js
@@ -0,0 +1,12 @@
+const operations = require('./modulos/functions');
+
+const resultSum = operations.sum(67, 3);
+console.log(`resultSuma: ${resultSum}`);
+
+const resultSubstract = operations.substract(7, 3);
+console.log(`resultSubstract: ${resultSubstract}`);
+
+const resultMultiply = operations.fnMultiply(4, 5);
+console.log(`resultMultiply: ${resultMultiply}`);
+
+
---
+++ 1-node/1.9-modulos/1.9.1-common-js/modulos/functions.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = {
+    sum: sum,
+    substract,
+    fnMultiply: multiply
+};
