e801b471d831275e75b83d14f033049dff98b3e0
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-07-10 19:15:19 -0600

    feat: arreglo dummy de canciones y endpoint GET / songs

--- 3-express/songs-api/src/index.js
+++ 3-express/songs-api/src/index.js
@@ -11,6 +11,20 @@ app.get('/hellow', (req, res) => {
     res.send('<h1> Hola desde express my friends!! </h1>');
 });
 
+const songs = [
+    { id: 1, name: 'Kilometros', artist: 'Los Caligaris' },
+    { id: 2, name: 'Closer', artist: 'The Chainsmokers' },
+    { id: 3, name: 'Solo a Terceros', artist: 'Panda' },
+    { id: 4, name: 'Danza Kuduro', artist: 'Don Omar' },
+];
+
+// GET: Obtener todas las canciones
+app.get('/songs', (req, res) => {
+    res.json(songs);
+});
+
+
+
 app.listen(port, () => {
     console.log(`servidor iniciado... en el puerto ${port}`.rainbow);
 });
